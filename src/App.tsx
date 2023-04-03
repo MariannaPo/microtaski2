e8d2e4c50e4e4a1d140e9c69a197094e2dc4433f
MariannaPo <mapodmasko@gmail.com>
2023-04-03 15:12:26 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,53 @@ import { Header } from './site/Header';
 import { Body } from './site/Body';
 import { Footer } from './site/Footer';
 import { NewComponent } from './site/NewComponent';
+import { ButtonChanel } from './site/ButtonChanel';
 
-const students=[
-  {id: 1, name: 'jriofj', age: 23},
-  {id: 2, name: 'jfrdegvwewrfj', age: 26},
-  {id: 3, name: 'l;a;ofj', age: 33},
-  {id: 4, name: 'hyew3yfj', age: 12},
-  {id: 5, name: 'jrfrjeifjre', age: 43},
-  {id: 1, name: 'jriofj', age: 23},
-  {id: 2, name: 'jfrdegvwewrfj', age: 26},
-  {id: 3, name: 'l;a;ofj', age: 33},
-  {id: 4, name: 'hyew3yfj', age: 12},
-  {id: 5, name: 'jrfrjeifjre', age: 43},
-]
 
-const topCars = [
-  {manufacturer:'BMW', model:'m5cs'},
-  {manufacturer:'Mercedes', model:'e63s'},
-  {manufacturer:'Audi', model:'rs6'}
-]
+
+// const students=[
+//   {id: 1, name: 'jriofj', age: 23},
+//   {id: 2, name: 'jfrdegvwewrfj', age: 26},
+//   {id: 3, name: 'l;a;ofj', age: 33},
+//   {id: 4, name: 'hyew3yfj', age: 12},
+//   {id: 5, name: 'jrfrjeifjre', age: 43},
+//   {id: 1, name: 'jriofj', age: 23},
+//   {id: 2, name: 'jfrdegvwewrfj', age: 26},
+//   {id: 3, name: 'l;a;ofj', age: 33},
+//   {id: 4, name: 'hyew3yfj', age: 12},
+//   {id: 5, name: 'jrfrjeifjre', age: 43},
+// ]
+
+// const topCars = [
+//   {manufacturer:'BMW', model:'m5cs'},
+//   {manufacturer:'Mercedes', model:'e63s'},
+//   {manufacturer:'Audi', model:'rs6'}
+// ]
 
 function App() {
+
+  const dj=(subscriber: string,age:number)=>{
+    console.log(subscriber, age);
+  }
+
+  const ds=(subscriber:string, age: number, address: string)=>{
+    console.log(subscriber, age, address);
+  }
+
+  const stb=()=>{
+    console.log('Im stupid button');
+    
+  }
+
   return (
     <div className="App">
     {/* <Header title={'BIG HEADER, WERY BIG'}/>
     <Body titleBody={'NEW BIG BODY'}/>
     <Footer titleFooter={'NEW WERY BIG FOOTER'}/> */}
-    <NewComponent students={students} cars={topCars}/>
+    {/* <NewComponent students={students} cars={topCars}/> */}
+    <ButtonChanel name={'GovnoChanel'} follower={()=>dj('Grisha', 45)}/>
+    <ButtonChanel name={'DermoChanel'} follower={()=>ds('Vitek', 76, 'Ostryaki')}/>
+    <ButtonChanel name={'ProstoButton'} follower={stb}/>
     </div>
   );
 }
---
+++ src/site/ButtonChanel.tsx
@@ -0,0 +1,49 @@
+import { type } from "os";
+import React from "react";
+
+type ButtonChanelPropsType={
+name: string,
+follower:()=>void
+}
+
+export const ButtonChanel=(props: ButtonChanelPropsType)=>{
+
+    const OnClickBut=()=>{
+        props.follower()
+    }
+
+//     const followerYT =()=>{
+//     console.log('Content govno!')
+// };
+
+// const followerTwit=()=>{
+//     console.log('napisana huita');
+    
+// }
+
+// const onClickHandler=(name: string)=>{
+//     console.log(name)
+// }
+
+// const foo1=(cifra: number)=>{
+// console.log(cifra);
+
+// }
+
+// const foo2=()=>{
+//     console.log(100200);
+    
+// }
+
+    return(
+        <div>
+            {/* <button onClick={()=>onClickHandler('eblan')}>MyYOUTUBEchanel-1</button>
+            <button onClick={()=>onClickHandler('mudila')}>MyTWITTERchanel-1</button>
+            <button onClick={()=>onClickHandler('govno')}>MyVK</button>
+            <button onClick={()=>onClickHandler('drochila')}>MyINST</button>
+            <button onClick={()=>foo1(100200)}>1</button>
+            <button onClick={foo2}>2</button> */}
+            <button onClick={OnClickBut}>{props.name}</button>
+        </div>
+    )
+}
